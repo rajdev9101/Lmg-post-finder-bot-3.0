6e1a86d45aa201a32067cfd2e997c3e2857bd103
rajdev9101 <devraj93957@gmail.com>
2025-01-10 23:42:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", "23900056"))
-API_HASH     = environ.get("API_HASH", "db7e21e638bc2359907814f4ed8b48a8")
+API_ID       = int(environ.get("API_ID", "27084955"))
+API_HASH     = environ.get("API_HASH", "91c88b554ab2a34f8b0c72228f06fc0b")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "")
 DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002419258367"))
-ADMIN        = int(environ.get("ADMIN", "7201053234"))
-CHANNEL      = environ.get("CHANNEL", "@movieupdatewithak")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002292323027"))
+ADMIN        = int(environ.get("ADMIN", "5804953849"))
+CHANNEL      = environ.get("CHANNEL", "https://t.me/+H-l9E4CaQ8pmZDBl")
